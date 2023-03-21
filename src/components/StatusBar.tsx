01820ff29b6a21e69c0de6a3ac4dcb592cf5a501
Rhys Tumelty <rhys@tumelty.co.uk>
2023-03-21 21:14:44 +0000

    Modified status bar to include a language indicater (WIP) and added some i18n

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,8 +1,31 @@
 import './styles/StatusBar.scss';
+import i18n from "i18next";
+
+import translationEN from "../lang/en.json";
+
+const resources = {
+    en: {
+        translation: translationEN
+    }
+};
 
 function StatusBar() {
+    i18n.init({
+        resources,
+        lng: "en",
+        keySeparator: false,
+
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+    
+
     return <div className="statusBar">
         <p className="statusBarItem">Photon Code Editor v1.0</p>
+        <p className='statusBarItem'>                </p>
+        <p className='statusBarItem'>{i18n.t("statusbar.language.javascript")}</p>
     </div>
 }
 
