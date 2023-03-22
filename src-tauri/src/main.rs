84ce67c83f9b6ffb758d9d8fd79f954807b2927f
Rhys Tumelty <rhys@tumelty.co.uk>
2023-03-22 18:59:18 +0000

    Started to implement file browser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use std::fs;
+
+fn get_files_in_directory(dir_path: &str) -> Result<Vec<String>, String> {
+    let dir_entries = fs::read_dir(dir_path)
+        .map_err(|e| format!("Failed to read directory: {}", e))?;
+
+    let files: Vec<String> = dir_entries
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+            if path.is_file() {
+                Some(path.to_string_lossy().into_owned())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(files)
 }
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[derive(Default)]
+struct MyState {
+  s: std::sync::Mutex<String>,
+  t: std::sync::Mutex<std::collections::HashMap<String, String>>,
+}
+// remember to call `.manage(MyState::default())`
+#[tauri::command]
+fn list_files_in_directory(dir_path: String) -> Result<Vec<String>, String> {
+    let result = get_files_in_directory(&dir_path);
+    let files_str = match result {
+        Ok(files) => files.join(", "),
+        Err(error) => format!("Error: {}", error),
+    };
+    println!("{}", files_str);
+
+    return get_files_in_directory(&dir_path);
+}
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![list_files_in_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/FileBrowser.tsx
+++ src/components/FileBrowser.tsx
@@ -1,10 +1,14 @@
 import './styles/FileBrowser.scss';
 import { enqueueSnackbar } from 'notistack';
+import { invoke } from '@tauri-apps/api/tauri'
 
 function FileBrowser() {
+    
+
     return ( <div className="fileBrowser">
         <p className="fileBrowserItem">File Browser</p>
-        <button className="fileBrowserItem" onClick={() => enqueueSnackbar<"error">('hi')}>Show snackbar</button>
+        <button className="fileBrowserItem" onClick={() => enqueueSnackbar('hi')}>Show snackbar</button>
+        <button onClick={() => invoke("list_files_in_directory", { dirPath: '/bin' })} />
     </div> )
 }
 
